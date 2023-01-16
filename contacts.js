a534bac7ca99d02582b4f6cec9e3d5dc19e8aee9
Ivan Vodopianov <vis3010@gmail.com>
2023-01-16 23:06:41 +0200

    hw-01 done

---
+++ contacts.js
@@ -0,0 +1,43 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { v4 } = require("uuid");
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contactIdToString = String(contactId);
+  const contactByID = contacts.find((contact) => contact.id === contactIdToString);
+  if (!contactByID) {
+    return null;
+  }
+  return contactByID;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const contactIdToString = String(contactId);
+  const index = contacts.findIndex((item) => item.id === contactIdToString);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = { id: v4(), name, email, phone };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
